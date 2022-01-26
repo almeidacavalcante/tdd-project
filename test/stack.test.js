48a2f67603736e64c84b0a96f118c224ada3dbae
almeidacavalcante <jalmeidacn@gmail.com>
2022-01-26 08:59:30 -0300

    initial commit

--- test/stack.test.js
+++ test/stack.test.js
@@ -1,25 +1,4 @@
-
-class Stack {
-    constructor() {
-        this.top = -1;
-        this.items = {};
-    }
-
-    push(value) {
-        this.top++;
-        this.items = {
-            ...this.items,
-            [`${this.top}`]: value,
-        }
-    }
-
-    pop() {
-        const top = this.items[this.top];
-        delete this.items[this.top];
-        this.top--;
-        return top;
-    }
-}
+const {Stack} = require('../src/Stack')
 
 describe('Testing the Stack implementation', () => {
     let stack;
